a819f9bd4f8499a3565957ee6b78506a15bbfd8d
Javier A Camacho Cuéllar <javierc@xinnux.com>
2021-03-19 09:33:26 -0600

    Primeras pruebas, se agrega imagen, name y botones next/prev

--- App.js
+++ App.js
@@ -1,11 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
+
+//Custom hooks
+import useFetch from './hooks/useFetch';
 
 export default function App() {
+
+  const [ pokemonId, setPokemonId ] = useState(1)
+  const { data, loading } = useFetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`);
+
+  const handlePressNext = () => {
+    setPokemonId(pokemonId + 1)
+  }
+
+  const handlePressBack = () => {
+    setPokemonId(pokemonId - 1)
+  }
+
   return (
+    
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {
+        loading ? 
+        <Text>
+          Cargando
+        </Text>
+        :
+        <View>
+          <Text> { data.name } </Text>
+          <Image style={ styles.img } source={{ uri: data.sprites.front_default }} />
+        </View>
+      }
+
+      <View>
+        <Button onPress={ handlePressBack } title="back"/>
+        <Button onPress={ handlePressNext } title="next"/>
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +50,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  img:{
+    width: 250,
+    height: 250
+  }
 });
